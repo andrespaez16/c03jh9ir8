16b9e9b3ded05b929a52200b49ccf6b69f3c4f06
andrespaez16 <kinkbmx216@gmail.com>
2018-11-08 18:14:19 -0500

    segunda vez

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,14 @@
 class ActorsController < ApplicationController
+
+def index
+    @actors=Actor.all 
+ end
+ 
+ private
+ 
+ def actor_params
+     params.require(:actor).permit(:image_url,:name,:bio,:alive,:death_date,:death_place,:birth_place,:birth_date)   
+ end
+ 
+ 
 end
